8336989d35eca20ba964e73af40b97ab256ec310
Timovzl <655426+Timovzl@users.noreply.github.com>
2020-08-19 20:03:44 +0200

    Fixed the RC version and a unit test.

--- Identities.Tests/IdGenerators/IncrementalIdGeneratorTests.cs
+++ Identities.Tests/IdGenerators/IncrementalIdGeneratorTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Architect.Identities.IdGenerators;
 using Xunit;
 
 namespace Architect.Identities.Tests.IdGenerators
